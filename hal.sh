4b96d3f6accf896461da775388d4d4458d66c79d
FrosT2k5 <androygaming@gmail.com>
2021-04-03 09:59:09 +0530

    frost bringup

--- hal.sh
+++ hal.sh
@@ -2,4 +2,4 @@
 
 alias gcl='git clone'
 
-cd ../../hardware/qcom-caf/msm8996/ && rm -rf audio media display && gcl https://github.com/android-eleven/hardware_qcom_media -b caf-msm8996-r media && gcl https://github.com/android-eleven/hardware_qcom_display -b caf-msm8996-r display && gcl https://github.com/android-eleven/hardware_qcom_audio -b caf-msm8996-r audio && cd -
+cd ../../hardware/qcom-caf/msm8996/ && rm -rf audio media display && gcl https://github.com/SuperCosmicBeing/media -b caf-msm8996-r media && gcl https://github.com/SuperCosmicBeing/display -b caf-msm8996-r display && gcl https://github.com/SuperCosmicBeing/audio -b caf-msm8996-r audio && cd -
